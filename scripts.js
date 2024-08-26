812fef055cabafa291a9605c64305807a7a998dd
ALP-INE <bagnaimirco@gmail.com>
2024-08-26 18:10:42 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// JavaScript code can go here if needed for interactive features
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded and parsed');
+});
